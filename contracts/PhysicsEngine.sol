da1b1f255193881259f0ab61615ecdb7c5315cd8
DevL0rd <dmhzmxn@gmail.com>
2022-03-31 17:20:08 +0700

    basic physics working

--- contracts/PhysicsEngine.sol
+++ contracts/PhysicsEngine.sol
@@ -9,6 +9,12 @@ contract PhysicsEngine {
         uint256 y;
     }
 
+    enum objectType {
+        circle,
+        rectangle,
+        line
+    }
+
     struct Object {
         uint256 id;
         vector2D pos;
@@ -16,6 +22,9 @@ contract PhysicsEngine {
         vector2D acc;
         int256 mass;
         bool isStatic;
+        objectType objectType;
+        uint256 radius;
+        vector2D size;
     }
 
     mapping(uint256 => Object) public objects;
@@ -26,7 +35,10 @@ contract PhysicsEngine {
         vector2D vel,
         vector2D acc,
         int256 mass,
-        bool isStatic
+        bool isStatic,
+        objectType objectType,
+        uint256 radius,
+        vector2D size
     );
     event ObjectUpdated(
         uint256 id,
@@ -34,7 +46,10 @@ contract PhysicsEngine {
         vector2D vel,
         vector2D acc,
         int256 mass,
-        bool isStatic
+        bool isStatic,
+        objectType objectType,
+        uint256 radius,
+        vector2D size
     );
 
     function createObject(
@@ -42,49 +57,116 @@ contract PhysicsEngine {
         vector2D vel,
         vector2D acc,
         int256 mass,
-        bool isStatic
-    ) public {
+        bool isStatic,
+        objectType objectType,
+        uint256 radius,
+        vector2D size
+    ) public returns (uint256) {
         uint256 id = objects.length;
-        objects[id] = Object(id, pos, vel, acc, mass, isStatic);
-        emit ObjectCreated(id, pos, vel, acc, mass, isStatic);
-        return objects[id];
+        Object object = Object({
+            id: id,
+            pos: pos,
+            vel: vel,
+            acc: acc,
+            mass: mass,
+            isStatic: isStatic,
+            objectType: objectType,
+            radius: radius,
+            size: size
+        });
+        objects[id] = object;
+        emit ObjectCreated(
+            id,
+            pos,
+            vel,
+            acc,
+            mass,
+            isStatic,
+            objectType,
+            radius,
+            size
+        );
+        return id;
+    }
+    world = {
+        gravity: vector2D({
+            x: 0,
+            y: -9.81
+        }),
+        friction: 0.5
     }
 
-    function applyForces(uint256 id) public {
-        Object obj = objects[id];
-        if (obj.isStatic) {
+    function applyGravity(uint256 id) public {
+        Object object = objects[id];
+        if (!object.isStatic) {
+            object.acc = vector2D({
+                x: object.acc.x + world.gravity.x,
+                y: object.acc.y + world.gravity.y
+            });
+        }
+    }
+
+    function updateObject(uint256 id, uint256 delta) public {
+        Object object = objects[id];
+        if (object.isStatic) {
             return;
         }
-        obj.vel.x += obj.acc.x;
-        obj.vel.y += obj.acc.y;
-        obj.pos.x += obj.vel.x;
-        obj.pos.y += obj.vel.y;
+        object.vel = vector2D({
+            x: object.vel.x + object.acc.x * delta,
+            y: object.vel.y + object.acc.y * delta
+        });
+        object.pos = vector2D({
+            x: object.pos.x + object.vel.x * delta,
+            y: object.pos.y + object.vel.y * delta
+        });
         emit ObjectUpdated(
             id,
-            obj.pos,
-            obj.vel,
-            obj.acc,
-            obj.mass,
-            obj.isStatic
+            object.pos,
+            object.vel,
+            object.acc,
+            object.mass,
+            object.isStatic,
+            object.objectType,
+            object.radius,
+            object.size
         );
     }
 
+    
     function tick() public {
         for (uint256 i = 0; i < objects.length; i++) {
-            applyForces(i);
-
-            emit ObjectUpdated(
-                obj.id,
-                obj.pos,
-                obj.vel,
-                obj.acc,
-                obj.mass,
-                obj.isStatic
-            );
+            applyGravity(i);
+            updateObject(i);
         }
     }
 
     constructor() public {
-        createObject(vector2D(0, 0), vector2D(0, 0), vector2D(0, 0), 1, false);
+        createObject(
+            vector2D({
+                x: 0,
+                y: 0
+            }),
+            vector2D({
+                x: 0,
+                y: 0
+            }),
+            vector2D({
+                x: 0,
+                y: 0
+            }),
+            1,
+            true,
+            objectType.circle,
+            1,
+            vector2D({
+                x: 1,
+                y: 1
+            })
+        );
+        //game loop
+        while (true) {
+            tick();
+            sleep(1000 / 60);
+        }
     }
 }
